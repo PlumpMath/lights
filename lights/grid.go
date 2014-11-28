3bdbdd5e6055f4031cd468c3d74329ed288c9677
Eric Fode <ericfode@gmail.com>
2014-11-28 23:47:51 +0000

    can now get the set of next moves

--- lights/grid.go
+++ lights/grid.go
@@ -10,6 +10,13 @@ import(
 type Game struct {
   grid [][]uint8 
   player uint8
+  width  uint
+  height uint
+}
+
+type Move struct {
+  x, y uint
+  value int 
 }
 
 func runeToUint(in rune)(out uint8){
@@ -22,7 +29,7 @@ func runeToUint(in rune)(out uint8){
   return 9
 }
 
-func ParseGrid(input string, width int, height int) [][]uint8 {
+func ParseGrid(input string, width uint, height uint) [][]uint8 {
   rows := strings.Split(input, "\n")
   
   slc := make([][]uint8, height)
@@ -30,19 +37,81 @@ func ParseGrid(input string, width int, height int) [][]uint8 {
     slc[i] = make([]uint8, width)
     fmt.Printf("%v", rows[i])
     for pos, char := range rows[i] {
-       
       slc[i][pos] = runeToUint(char) 
     }
   }
   return slc 
 }
 
-func ParseGame(input string, width int, height int) Game {
-  playerStr := strings.Split(input, "\n")[0]
+func ParseGame(input string) Game {
+  lines     := strings.Split(input, "\n")
+  playerStr := lines[0]
+  height    := uint(len(lines[1:]))
+  width     := uint(len(lines[1]))
   player, err := strconv.Atoi(playerStr)
   if err != nil {
     log.Fatal(err)
   } 
-  grid := ParseGrid(input, width, height)
-  return Game{grid, uint8(player)}
+  grid := ParseGrid(strings.Join(lines[1:],"\n"), width, height)
+  return Game{grid, uint8(player), width, height}
+}
+
+func flip(bit uint8) uint8{
+  if bit == 1{
+    return 0
+  } else {
+    return 1
+  }
+  
+}
+
+func (game Game) Blast(x,y uint)(Game){
+  g := game.copyGame()
+  g.grid[y][x] = 0
+  g.grid[y][x+1] = flip(game.At(x+1,y)) 
+  g.grid[y+1][x] = flip(game.At(x,y+1)) 
+  return g
+}
+
+func (game Game) BlastValue(x,y uint)(int){
+  return int(game.At(x,y) + game.At(x+1,y) + game.At(x,y+1))
+}
+
+func (game Game) CanBlast(x,y uint)(bool){
+  fmt.Println(x, y, game.At(x,y))
+  
+  if game.At(x,y) == 0 {
+    return false
+  } else {
+    return true
+  }
+}
+
+func (game Game) PossibleMoves()([]Move){
+  fmt.Printf("%v", game)
+  slc := make([]Move, 0)
+  for x := uint(0); x < game.width; x++ {
+    for y := uint(0); y < game.height; y++ {
+      if game.CanBlast(uint(x),uint(y)) && game.BlastValue(uint(x), uint(y) ) > 0 {
+        slc =  append(slc,Move{uint(x),uint(y), game.BlastValue(uint(x),uint(y))})
+      }
+    }
+  }
+  return slc
+}
+func (game Game) copyGame()(Game){
+  slc := make([][]uint8, game.width)  
+  for x := range(game.grid){
+    slc[x] = make([]uint8, game.height)
+    copy(slc[x],game.grid[x])
+  }
+  return Game{slc, game.player, game.width, game.height} 
+}
+
+func (game Game) At(x,y uint)(uint8){
+  if x < game.width && y < game.height {
+    return game.grid[y][x]
+  } else {
+    return 0
+  }
 }
--- lights/grid_test.go
+++ lights/grid_test.go
@@ -2,6 +2,7 @@ package lights
 
 import ("testing"
         . "gopkg.in/check.v1"
+        "fmt"
        )
 func Test(t *testing.T) { TestingT(t) }
 
@@ -9,9 +10,51 @@ type LSuite struct{
    Grid [][]uint8
    gridIn string 
    gameIn string
+   smallGrid [][]uint8
+   smallGridBlast [][]uint8
+   smallGame Game
+   smallGridMoves  []Move
    game Game
 }
+func (s* LSuite) SetUpTest(c *C){
+  s.Grid = [][]uint8{
+    {0,1,1,0,1,1,0,0},
+    {0,1,0,1,0,1,0,1},
+    {1,0,1,1,1,1,0,0},
+    {0,0,0,0,1,1,1,1},
+    {1,0,1,0,1,0,1,0},
+    {0,0,0,0,0,0,0,1},
+    {1,1,1,0,0,0,1,1},
+    {0,0,1,0,1,0,0,0},
+  }
+  s.smallGrid = [][]uint8{
+    {0,1,1,1},
+    {0,1,0,0},
+    {0,1,1,0},
+    {0,1,1,0},
+  } 
+  
+  s.smallGridMoves = []Move{
+    Move{1,0,3},
+    Move{1,1,2},
+    Move{1,2,3},
+    Move{1,3,2},
+    Move{2,0,2},
+    Move{2,2,2},
+    Move{2,3,1},
+    Move{3,0,1},
+  } 
+  
+  s.smallGridBlast = [][]uint8{
+    {0,0,0,1},
+    {0,0,0,0},
+    {0,1,1,0},
+    {0,1,1,0},
+  } 
 
+    s.game = Game{s.Grid,1, 8,8}
+    s.smallGame = Game{s.smallGrid,1, 4,4}
+}
 func (s* LSuite) SetUpSuite(c *C){
   s.gameIn = `1
 01101100
@@ -31,26 +74,48 @@ func (s* LSuite) SetUpSuite(c *C){
 00000001
 11100011
 00101000`
-  
-  s.Grid = [][]uint8{
-    {0,1,1,0,1,1,0,0},
-    {0,1,0,1,0,1,0,1},
-    {1,0,1,1,1,1,0,0},
-    {0,0,0,0,1,1,1,1},
-    {1,0,1,0,1,0,1,0},
-    {0,0,0,0,0,0,0,1},
-    {1,1,1,0,0,0,1,1},
-    {0,0,1,0,1,0,0,0},
-  }
-  
-  s.game = Game{s.Grid,1}
+
 }
 
 var _ = Suite(&LSuite{})
 
 
-func (s *LSuite) TestParse(c *C){
+func (s *LSuite) TestParseGrid(c *C){
   output := ParseGrid(s.gridIn ,8,8)
   c.Assert(output, DeepEquals, s.Grid)
+}
+
+func (s *LSuite) TestParseGame(c *C){
+  output := ParseGame(s.gameIn)
+  c.Assert(output, DeepEquals, s.game)
+}
+
+func (s *LSuite) TestCanBlastFalse(c *C){
+  output := s.smallGame.CanBlast(3,1)
+  c.Assert(output, Equals, false)
+}
+
+func (s *LSuite) TestCanBlastTrue(c *C){
+  output := s.smallGame.CanBlast(3,0)
+  c.Assert(output, Equals, true)
+}
+
+func (s *LSuite) TestAt(c *C){
+  output := s.smallGame.At(0,0)
+  c.Assert(output, Equals, uint8(0x0))
+  output = s.smallGame.At(3,0)
+  c.Assert(output, Equals, uint8(0x1))
+}
+
+func (s *LSuite) TestPossibleMoves(c *C) {
+  output := s.smallGame.PossibleMoves()
+  c.Assert(output, DeepEquals, s.smallGridMoves)
+}
+
+func (s *LSuite) TestBlast(c *C){
+  output := s.smallGame.Blast(1,0) 
+  c.Assert(output.grid, DeepEquals, s.smallGridBlast)
+  c.Assert(s.smallGame.grid, DeepEquals, s.smallGrid)
+  fmt.Println(s.smallGame)
 }
 
